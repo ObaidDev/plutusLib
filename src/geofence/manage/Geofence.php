8a6bd1f5e95db85dca283d7f1e35f6fc702ba55e
plutus360 <plutus360.com@gmail.com>
2024-10-09 14:00:44 +0100

    Update some mistakes

--- src/geofence/manage/Geofence.php
+++ src/geofence/manage/Geofence.php
@@ -90,7 +90,7 @@ final class Geofence implements GeofenceInterface {
 
     function getGeofences(GeofenceDto $geofenceDto , $userToken) : array{
         $url = CurlHelper::getEndpointUrl(__DIR__."/../../../config/endpoints.json" , "geofence") ;
-        $url = $url . "/".join(",",$geofenceDto->getIds()) ;
+        $url = $url . "/".($geofenceDto->getIds() != null ?join(",",$geofenceDto->getIds()) : "all") ;
         $url = $url . ($geofenceDto->getFields() != null ? "?fields=".join("," , $geofenceDto->getFields()) : null) ;
 
         return CurlHelper::excuteCurl(
